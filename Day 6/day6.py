956b9f07ceffd45ddc3801759ad4bd0bd7f2dab9
Finn Saile <86435297+finnsaile@users.noreply.github.com>
2022-12-06 17:56:45 +0100

    Add oneline solution for day 6

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,8 +1,6 @@
 import numpy as np
 line = list(open('input.txt').read().strip())
 
-quadrupel = []
-
 for i in range(0, len(line) - 3):
     quadrupel = line[i:i+4]
     if len(np.unique(quadrupel)) == 4:
@@ -14,3 +12,6 @@ for i in range(0, len(line) - 13):
     if len(np.unique(quadrupel)) == 14:
         print(f"1. {i + 14}")
         break
+
+# oneline solution
+[print(f'{x + 1}. {[i + y for i in range(0, len(line) - y - 1) if len(np.unique(line[i:i + y])) == y][0]}') for (x, y) in enumerate([4, 14]) for line in [list(open('input.txt').read().strip())]]
